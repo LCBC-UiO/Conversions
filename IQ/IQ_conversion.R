32db4be2cc24bc3c9f97942621f39d7f52cce9e6
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2017-11-24 12:43:55 +0100

    simplified a little

--- IQ/IQ_conversion.R
+++ IQ/IQ_conversion.R
@@ -1,22 +1,20 @@
 wasi.conversion = function(IN, nSubtests=2) {
   require(tidyverse); require(readxl)
- 
-  # duplicate array to work with
-  Int = IN
   
   # Get correct conversion table
   type = paste0("WASI.Full.", nSubtests ,"tests")
   wasi.tb = as.matrix(read_excel("conversion_tables.xlsx", sheet =type, col_names = FALSE))
   
   #set t-values outside allowed range to 999
-  Int[IN < min(wasi.tb[,1])] =  999
-  Int[IN > max(wasi.tb[-length(wasi.tb[,1]),1])] =  999
+  Int = IN %>% 
+    ifelse(. < min(wasi.tb[,1]) | . > max(wasi.tb[-length(wasi.tb[,1]),1]), 
+           999, .) 
   
   #Index the t-scores to the wasi table
   inds = match(Int, wasi.tb[,1])
   
   #Get the values from the wasi table at the extrected indeces
-  OUT = wasi.tb[inds,2]
+  OUT = wasi.tb[match(Int, wasi.tb[,1]),2]
   
   return(OUT)
 }
