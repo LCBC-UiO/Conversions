c7979d89d5acfec5907bfc40fb33ce9b2aacf3e2
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-03-17 09:41:17 +0100

    fixed bmi

--- R/bmi-functions.R
+++ R/bmi-functions.R
@@ -13,8 +13,12 @@
 #' @family bmi functions
 #' @examples
 #' bmi_calc(176, 72)
-bmi_calc <- function(height, weight){
-  weight/(height/100)^2
+bmi_calc <- function(height, weight, unit = list(height = "cm", 
+                                                 weight = "kg")){
+  
+  corrected <- bmi_unit(height, weight, unit)
+  
+  corrected$weight/corrected$height^2
 }
 
 #' Calculate BMI
@@ -30,6 +34,25 @@ bmi_calc <- function(height, weight){
 #' @family bmi functions
 #' @examples
 #' bmi_calc2(176, 72)
-bmi_calc2 <- function(height, weight){
-  weight/(height/100)^2.5
+bmi_calc2 <- function(height, weight, unit = list(height = "cm", 
+                                                  weight = "kg")){
+  
+  corrected <- bmi_unit(height, weight, unit)
+  
+  corrected$weight/corrected$height^2.5
+}
+
+bmi_unit <- function(height, weight, unit = list(height = "cm", 
+                                                 weight = "kg")){
+  
+  if(missing(height)) stop("height is missing", call.=FALSE)
+  if(missing(weight)) stop("weight is missing", call.=FALSE)
+  
+  unit_height <- match.arg(unit$height, c("m", "cm"))
+  unit_weight <- match.arg(unit$weight, c("g", "kg"))
+  
+  if(unit_height == "cm") height = height/100
+  if(unit_weight == "g")  weight = weight/100
+  
+  return(list(height = height, weight = weight))
 }
--- tests/testthat/test-bmi-functions.R
+++ tests/testthat/test-bmi-functions.R
@@ -1,4 +1,4 @@
-test_that("bmi_calc2 works", {
+test_that("bmi_calc2 bmi_calc works", {
   expect_equivalent(bmi_calc2(176, 72), 17.52067, tolerance = .0001)
   expect_equivalent(bmi_calc(176, 72), 23.2438, tolerance = .0001)
   
@@ -6,8 +6,8 @@ test_that("bmi_calc2 works", {
                    weight = c(72, 45, 110))
   
   ret_dt <- mutate(dt, 
-         bmi = bmi_calc(height, weight),
-         bmi2 = bmi_calc2(height, weight))
+                   bmi = bmi_calc(height, weight),
+                   bmi2 = bmi_calc2(height, weight))
   
   expect_equal(ret_dt$bmi,
                c(24.33, 16.731, 29.83940), 
@@ -16,5 +16,25 @@ test_that("bmi_calc2 works", {
   expect_equal(ret_dt$bmi2,
                c(18.557, 13.0648, 21.5347), 
                tolerance = .001)
+  
+})
+
+
+
+test_that("bmi_unit works", {
+  expect_error(bmi_unit(), "missing")
+  expect_error(bmi_unit(height = 200), "missing")
+  
+  expect_equivalent(bmi_unit(200, 80),
+                    list(height = 2, weight = 80))
+  
+  expect_equivalent(bmi_unit(200, 80, unit = list(height = "cm", weight = "kg")),
+                    list(height = 2, weight = 80))
+  
+  expect_equivalent(bmi_unit(2, 80, unit = list(height = "m", weight = "kg")),
+                    list(height = 2, weight = 80))
+  
+  expect_equivalent(bmi_unit(2, 8000, unit = list(height = "m", weight = "g")),
+                    list(height = 2, weight = 80))
   
 })
